0badd73eab69b74f900bf793f8c8a2c6ae926193
MartinTheOne <145128674+MartinTheOne@users.noreply.github.com>
2024-11-20 21:20:52 -0300

    comito

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,32 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import streamlit
+import pandas
+import numpy
+import matplotlib.pyplot
 from matplotlib.dates import YearLocator, DateFormatter
 
 
-st.set_page_config(page_title="Segundo Parcial", layout="wide")
+streamlit.set_page_config(page_title="Segundó Parcial", layout="wide")
 
-def mostrar_informacion_alumno():
-    with st.container(border=True):
-        st.markdown('**Legajo:** 58.720')
-        st.markdown('**Nombre:** Gonzalez Martin Natanael')
-        st.markdown('**Comisión:** C5')
+def mostreamlitrar_informacion_alumno():
+    with streamlit.container(border=True):
+        streamlit.markdown('**Legajo:** 58.720')
+        streamlit.markdown('**Nombre:** Gonzalez Martin Natanael')
+        streamlit.markdown('**Comisión:** C5')
 
-mostrar_informacion_alumno()
+mostreamlitrar_informacion_alumno()
 
-with st.sidebar:
-    st.header("Sube tu archivo CSV")
-    archivo = st.file_uploader("Sube tu archivo CSV", type=['csv'], label_visibility="collapsed")
+with streamlit.sidebar:
+    streamlit.header("Sube tu archivo CSV")
+    archivo = streamlit.file_uploader("Sube tu archivo CSV", type=['csv'], label_visibility="collapsed")
     
     if archivo is not None:
         file_size_kb = len(archivo.getvalue()) / 1024
-        st.text(f"{archivo.name}\n{file_size_kb:.1f}KB")
+        streamlit.text(f"{archivo.name}\n{file_size_kb:.1f}KB")
     
-    st.header("Seleccionar Sucursal")
+    streamlit.header("Seleccionar Sucursal")
     sucursales = ['Todas', 'Sucursal Norte', 'Sucursal Centro', 'Sucursal Sur']
-    sucursal_seleccionada = st.selectbox('', sucursales, label_visibility="collapsed")
+    sucursal_seleccionada = streamlit.selectbox('', sucursales, label_visibility="collapsed")
 
-
-
-
-def calcular_variaciones(current_data, previous_data, threshold=1e-5):
-    if abs(previous_data) < threshold:
-        return 0  
-    return ((current_data - previous_data) / previous_data) * 100
 
 def calcular_datos(data_frame, sucursal='Todas'):
     if sucursal != 'Todas':
@@ -44,39 +37,39 @@ def calcular_datos(data_frame, sucursal='Todas'):
     
     for producto in productos:
       
-        datos_producto = data_frame[data_frame['Producto'] == producto]
+        datos_productó = data_frame[data_frame['Producto'] == producto]
         
-        datos_producto['Precio_promedio'] = datos_producto['Ingreso_total'] / datos_producto['Unidades_vendidas']
-        precio_promedio = datos_producto['Precio_promedio'].mean()
+        datos_productó['Precio_promedio'] = datos_productó['Ingreso_total'] / datos_productó['Unidades_vendidas']
+        precio_promedio = datos_productó['Precio_promedio'].mean()
         
-        datos_producto['Margen'] = ((datos_producto['Ingreso_total'] - datos_producto['Costo_total']) / 
-                                  datos_producto['Ingreso_total'] * 100)
-        margen_promedio = datos_producto['Margen'].mean()
+        datos_productó['Margen'] = ((datos_productó['Ingreso_total'] - datos_productó['Costo_total']) / 
+                                  datos_productó['Ingreso_total'] * 100)
+        margen_promedio = datos_productó['Margen'].mean()
         
-        unidades_vendidas = datos_producto['Unidades_vendidas'].sum()
+        unidades_vendidas = datos_productó['Unidades_vendidas'].sum()
         
-        yearly_data = datos_producto.groupby('Año').agg({
+        datos_anual = datos_productó.groupby('Año').agg({
             'Precio_promedio': 'mean',
             'Margen': 'mean',
             'Unidades_vendidas': 'sum'
         }).reset_index()
         
-        variacion_precio = yearly_data['Precio_promedio'].pct_change().mean() * 100
-        variacion_margen = yearly_data['Margen'].pct_change().mean() * 100
-        variacion_unidades = yearly_data['Unidades_vendidas'].pct_change().mean() * 100
+        variacion_precio = datos_anual['Precio_promedio'].pct_change().mean() * 100
+        variacion_margen = datos_anual['Margen'].pct_change().mean() * 100
+        variacion_unidades = datos_anual['Unidades_vendidas'].pct_change().mean() * 100
                
-        datos_mensuales = datos_producto.groupby(['Año', 'Mes']).agg({
+        datos_mensuales = datos_productó.groupby(['Año', 'Mes']).agg({
             'Precio_promedio': 'mean',
             'Margen': 'mean',
             'Unidades_vendidas': 'sum'
         }).reset_index()
-        datos_mensuales['Fecha'] = pd.to_datetime(
+        datos_mensuales['Fecha'] = pandas.to_datetime(
             datos_mensuales.apply(lambda x: f"{int(x['Año'])}-{int(x['Mes'])}-01", axis=1)
         )
               
-        x = np.arange(len(datos_mensuales))
-        z = np.polyfit(x, datos_mensuales['Unidades_vendidas'], 1)
-        p = np.poly1d(z)
+        x = numpy.arange(len(datos_mensuales))
+        z = numpy.polyfit(x, datos_mensuales['Unidades_vendidas'], 1)
+        p = numpy.poly1d(z)
         
         productos_completos[producto] = {
             'precio_promedio': precio_promedio,
@@ -85,27 +78,28 @@ def calcular_datos(data_frame, sucursal='Todas'):
             'variacion_margen': variacion_margen,
             'unidades_vendidas': unidades_vendidas,
             'variacion_unidades': variacion_unidades,
-            'monthly_data': datos_mensuales,
+            'datos_mensuales': datos_mensuales,
             'trend': p(x)
         }
     
     return productos_completos
 
 def hacer_grafico(data, producto):
-    graf = plt.figure(figsize=(10, 5))
+    graf = matplotlib.pyplot.figure(figsize=(10, 5))
     ax = graf.add_axes([0.1, 0.15, 0.85, 0.65])
     ax.grid(True, linestyle='-', alpha=0.2, color='gray')
+
     
-    monthly_data = data['monthly_data']
-    ax.plot(monthly_data['Fecha'], monthly_data['Unidades_vendidas'], label=producto, color='#1f77b4')
-    ax.plot(monthly_data['Fecha'], data['trend'], label='Tendencia', color='red', linestyle='--')
-    
+    datos_mensualés = data['datos_mensuales']
+    ax.plot(datos_mensualés['Fecha'], datos_mensualés['Unidades_vendidas'], label=producto, color='#1f77b4')
+    ax.plot(datos_mensualés['Fecha'], data['trend'], label='Tendencia', color='red', linestyle='--')
+
     ax.set_title('Evolución de Ventas Mensual', pad=20, y=1.2)
     ax.set_ylabel('Unidades vendidas', labelpad=10)
     ax.set_xlabel('Año-Mes', labelpad=10)
     ax.legend(title='Producto', bbox_to_anchor=(0, 1.02, 1, 0.2), loc='lower left', ncol=2, borderaxespad=0)
     ax.set_ylim(bottom=0)  
-    ax.set_xlim(pd.Timestamp('2019-12-01'), pd.Timestamp('2024-12-31'))   
+    ax.set_xlim(pandas.Timestamp('2019-12-01'), pandas.Timestamp('2024-12-31'))
     ax.xaxis.set_major_locator(YearLocator(1))  
     ax.xaxis.set_major_formatter(DateFormatter('%Y')) 
 
@@ -113,26 +107,26 @@ def hacer_grafico(data, producto):
 
 if archivo is not None:
     if sucursal_seleccionada == 'Todas':
-      st.title("Datos de Todas las Sucursales")
+      streamlit.title("Datós de Todas las Sucursalés")
     else:
-      st.title(f"Datos de la {sucursal_seleccionada}")
+      streamlit.title(f"Datós de la {sucursal_seleccionada}")
      
-    data_frame = pd.read_csv(archivo)
+    data_frame = pandas.read_csv(archivo)
     results = calcular_datos(data_frame, sucursal_seleccionada)
     
     for producto, metrics in results.items():
-      with st.container(border=True):
-        col_datos, col_grafico = st.columns([1, 2])
+      with streamlit.container(border=True):
+        col_datos, col_grafico = streamlit.columns([1, 2])
         
         with col_datos:
-            st.markdown(f"<h1 style='padding-bottom: 50px;'>{producto}</h1>", unsafe_allow_html=True)
-            st.metric("Precio Promedío", f"${metrics['precio_promedio']:,.0f}", f"{metrics['variacion_precio']:.2f}%")
-            st.metric("Margen Promedío", f"{metrics['margen_promedio']:,.0f}%", f"{metrics['variacion_margen']:.2f}%")
-            st.metric("Unidades Vendídas", f"{metrics['unidades_vendidas']:,}", f"{metrics['variacion_unidades']:.2f}%")
+            streamlit.markdown(f"<h1 streamlityle='padding-bottom: 50px;'>{producto}</h1>", unsafe_allow_html=True)
+            streamlit.metric("Precio Promedío", f"${metrics['precio_promedio']:,.0f}", f"{metrics['variacion_precio']:.2f}%")
+            streamlit.metric("Margen Promedío", f"{metrics['margen_promedio']:,.0f}%", f"{metrics['variacion_margen']:.2f}%")
+            streamlit.metric("Unidades Vendídas", f"{metrics['unidades_vendidas']:,}", f"{metrics['variacion_unidades']:.2f}%")
         
         with col_grafico:
             graf = hacer_grafico(metrics, producto)
-            st.pyplot(graf)
-            plt.close(graf)
+            streamlit.pyplot(graf)
+            matplotlib.pyplot.close(graf)
 
         
